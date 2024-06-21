50e6e1a0c633ca1d1827db0140d781fc9860af3a
github-action-benchmark <github@users.noreply.github.com>
2024-06-21 19:31:51 +0000

    add Benchmark (customBiggerIsBetter) benchmark result for 356174b0ab2fe036a76b5e5c34f0bb79eb2e9584

--- evaluations/data.js
+++ evaluations/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1718998296812,
+  "lastUpdate": 1718998311009,
   "repoUrl": "https://github.com/jmontleon/kai",
   "entries": {
     "Benchmark": [
@@ -28,6 +28,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "ChatIBMGenAI_meta-llama_llama-3-70b-instruct_example_a",
             "value": "43.0",
+            "unit": "Match"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "jmontleo@redhat.com",
+            "name": "Jason Montleon",
+            "username": "jmontleon"
+          },
+          "committer": {
+            "email": "jmontleo@redhat.com",
+            "name": "Jason Montleon",
+            "username": "jmontleon"
+          },
+          "distinct": true,
+          "id": "356174b0ab2fe036a76b5e5c34f0bb79eb2e9584",
+          "message": "Add Evaluation GH Action\n\nSigned-off-by: Jason Montleon <jmontleo@redhat.com>",
+          "timestamp": "2024-06-21T15:27:58-04:00",
+          "tree_id": "537e705869de30e2ca40a21cff23ec39c059e474",
+          "url": "https://github.com/jmontleon/kai/commit/356174b0ab2fe036a76b5e5c34f0bb79eb2e9584"
+        },
+        "date": 1718998310451,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "ChatIBMGenAI_mistralai_mixtral-8x7b-instruct-v01_example_a",
+            "value": "295.0",
             "unit": "Match"
           }
         ]
