1820b0c7956aab58c3b4f3998c1b5ac360878cd0
github-action-benchmark <github@users.noreply.github.com>
2024-10-25 14:03:31 +0000

    add Benchmark (customBiggerIsBetter) benchmark result for cb333eaf2e2e2051a28e8d712a655961f0ac971a

--- evaluations/data.js
+++ evaluations/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1729863937818,
+  "lastUpdate": 1729865011891,
   "repoUrl": "https://github.com/jmontleon/kai",
   "entries": {
     "Benchmark": [
@@ -3391,6 +3391,35 @@ window.BENCHMARK_DATA = {
         "benches": [
           {
             "name": "ChatIBMGenAI_deepseek-ai_deepseek-coder-33b-instruct_example_a",
+            "value": "",
+            "unit": "Match"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "jmontleo@redhat.com",
+            "name": "Jason Montleon",
+            "username": "jmontleon"
+          },
+          "committer": {
+            "email": "jmontleo@redhat.com",
+            "name": "Jason Montleon",
+            "username": "jmontleon"
+          },
+          "distinct": true,
+          "id": "cb333eaf2e2e2051a28e8d712a655961f0ac971a",
+          "message": ":sparkles: Add Binary Build and Publish Workflow\n\nSigned-off-by: Jason Montleon <jmontleo@redhat.com>",
+          "timestamp": "2024-10-25T10:00:53-04:00",
+          "tree_id": "2e134f6ecc277c0940b27137b68a13223c63955a",
+          "url": "https://github.com/jmontleon/kai/commit/cb333eaf2e2e2051a28e8d712a655961f0ac971a"
+        },
+        "date": 1729865011513,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "ChatIBMGenAI_codellama_codellama-34b-instruct_example_a",
             "value": "",
             "unit": "Match"
           }
